ae1d0ce84397fb8e3200fafdd252efc08c219423
afk552 <ez552@mail.ru>
2022-09-10 01:41:11 +0300

    Sorting of array added

--- program.py
+++ program.py
@@ -4,6 +4,11 @@ arr = []
 for i in range (3):
     arr.append(random.randint(1,100))
 
+for i in range(len(arr)):
+    for j in range(len(arr) - 1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
 res = sum(arr)
 
 print (res)
